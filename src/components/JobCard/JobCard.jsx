bd38c6b8905531f0b54df280a1f75f7d4255fa9f
Mauricio Giana <86989554+MauricioGiana@users.noreply.github.com>
2022-01-06 22:01:55 -0300

    borrados imports no usados

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { getJobs } from "../../controllers";
 import {Link} from "react-router-dom";
 import styles from "./JobCard.module.css";
-const image = "https://cursosvirtualesgratis.com/wp-content/uploads/2020/09/donde-aprender-carpinteria-cursos-de-carpinteria.jpg"
 
 export default function JobCard(props) {
     return (
