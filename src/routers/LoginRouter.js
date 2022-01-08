2f87ee18b04b90e96ede81a774b2a5f5a00d722e
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-01-07 22:49:30 -0300

    ruta PostDetail sin conectar al back

--- src/routers/LoginRouter.js
+++ src/routers/LoginRouter.js
@@ -3,14 +3,16 @@ import { Routes, Route } from "react-router-dom";
 /* import { HomeScreen } from "../components/home/HomeScreen"; */
 import Home from "../components/Home/Home";
 import { ProfileDetails } from "../components/ProfileDetails/ProfileDetails";
+import PostDetail from "../components/PostDetail/PostDetail";
 
 export const LoginRouter = () => {
   return (
     <div>
       <div>
         <Routes>
-          <Route path="home" element={<Home type="jobs" />} />
+          <Route path="home" element={<Home type="posts" />} />
           <Route path="profile" element={<ProfileDetails />} />
+          <Route path="post/:id" element={<PostDetail />} />
           {/* {Todas las rutas privadas deben ir aquí} */}
         </Routes>
       </div>
