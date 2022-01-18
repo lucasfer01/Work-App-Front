3f498c9cc3924c6711276328aa10fe40482a2303
Martin Morlio <tincho20012017@gmail.com>
2022-01-18 15:34:42 -0300

    mail

--- src/routers/LoginRouter.js
+++ src/routers/LoginRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../components/home/Home";
+import Home from "../components/home/home.jsx";
 import { ProfileDetails } from "../components/ProfileDetails/ProfileDetails";
 import PostDetail from "../components/PostDetail/PostDetail";
 import FormEmpleador from "../components/FormEmpleador/FormEmpleador";
