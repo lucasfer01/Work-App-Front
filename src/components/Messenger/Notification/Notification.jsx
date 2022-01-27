71bbe945c144d367e32a35480a87d9946f2e4433
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-01-27 04:40:23 -0300

    notificaciones

--- src/components/Messenger/Notification/Notification.jsx
+++ src/components/Messenger/Notification/Notification.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Notification.css";
 
 const imgDefault = "https://firebasestorage.googleapis.com/v0/b/react-eccomerce-979a7.appspot.com/o/Categorias%2FWorkingapp.jpg?alt=media&token=09c05864-0fca-4fed-8c69-e44c328b2d0e"
@@ -6,7 +7,7 @@ const imgDefault = "https://firebasestorage.googleapis.com/v0/b/react-eccomerce-
 const Notification = ({post, jobs}) => {
     let photo = post?.post_photo.length ? post?.post_photo[0] : imgDefault
     let job = jobs?.length ? jobs[0] : "servicios que puedes ofrecer"
-
+    const navigate = useNavigate();
     return (
         <div className={'message'}>
             <div className='messageTop'>
@@ -15,7 +16,9 @@ const Notification = ({post, jobs}) => {
                 alt=""
                 />
                 <p className='messageText'>Alguien necesita {job} </p>
+                <button onClick={() => navigate(`/post/${post.post_id}`)} >
                 <p className='messageText'>{post?.post_title}</p>
+                </button>
             </div>
             <div className='messageBottom'>{post?.createdAt}</div>
         </div>
